7e91598b88ccdebabd0fa3fdc69ccf80ed7017b6
reginasperea <89654937+reginasperea@users.noreply.github.com>
2021-08-27 20:19:08 -0500

    Add files via upload
    Termine problema 1

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,7 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: ")) 
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    m = (y2-y1) / (x2-x1)
+    print("Pendiente:", m)
 
 if __name__ == '__main__':
     main()
