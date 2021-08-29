1b27263902845238885310949cddeff94f308d2a
reginasperea <89654937+reginasperea@users.noreply.github.com>
2021-08-29 13:42:36 -0500

    Add files via upload
    Termine problema 4

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    import math
+    palabras = int(input("Dame el número de palabras: "))
+    if palabras <= 475:
+        costo = 60 * 0.90
+        print("El costo de la publicación es:", costo)
+    else:
+        costo = ((math.ceil(palabras / 475)) * 60) * 0.90
+        print("El costo de la publicación es:", costo)
 
 if __name__ == '__main__':
     main()
