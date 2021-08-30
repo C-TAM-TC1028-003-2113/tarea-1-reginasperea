b63a5a631017685ac9d73f481b9fe85257d7a6b5
reginasperea <89654937+reginasperea@users.noreply.github.com>
2021-08-29 20:25:51 -0500

    Add files via upload
    Termine el ultimo ejercicio

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,7 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
+    import math
+    harina = float(input("Dame la harina en gramos: "))
+    levadura = float(math.ceil((harina/1000) * 50))
+    print("Necesitas estos gramos de levadura:", levadura)
 
 if __name__ == '__main__':
     main()
